3b5b115714226ecc393af0f26fc0fa2f9de7b9d6
ROMEO <empereurayakawa@gmail.com>
2025-09-03 23:51:10 +0100

    Update callad.js

--- scripts/cmds/callad.js
+++ scripts/cmds/callad.js
@@ -34,17 +34,17 @@ module.exports = {
 			noAdmin: "Hiện tại bot chưa có admin nào"
 		},
 		en: {
-			missingMessage: "Please enter the message you want to send to admin",
+			missingMessage: "🎧Please enter the message you want to send to my admin এꔰꜛ.`ROMEO`.ꜛꗄꔰ🪶😌",
 			sendByGroup: "\n- Sent from group: %1\n- Thread ID: %2",
 			sendByUser: "\n- Sent from user",
 			content: "\n\nContent:\n─────────────────\n%1\n─────────────────\nReply this message to send message to user",
-			success: "Sent your message to %1 admin successfully!\n%2",
-			failed: "An error occurred while sending your message to %1 admin\n%2\nCheck console for more details",
-			reply: "📍 Reply from admin %1:\n─────────────────\n%2\n─────────────────\nReply this message to continue send message to admin",
-			replySuccess: "Sent your reply to admin successfully!",
+			success: "🎧Sent your message to %1 admin successfully🪶😌!\n%2",
+			failed: "🎧❌An error occurred while sending your message to %1 admin🪶🧘🏻‍♂️\n%2\nCheck console for more details",
+			reply: "🎧📍 Reply from admin %1🪶😌:\n─────────────────\n%2\n─────────────────\nReply this message to continue send message to admin",
+			replySuccess: "🎧Sent your reply to admin এꔰꜛ.`ROMEO`.ꜛꗄꔰ successfully🪶🎧!",
 			feedback: "📝 Feedback from user %1:\n- User ID: %2%3\n\nContent:\n─────────────────\n%4\n─────────────────\nReply this message to send message to user",
 			replyUserSuccess: "Sent your reply to user successfully!",
-			noAdmin: "Bot has no admin at the moment"
+			noAdmin: "🎧Bot has no admin at the moment🪶🧘🏻‍♂️"
 		}
 	},
 
